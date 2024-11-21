6af4c2d0746229a6c70eb7102f89713e32b51383
jyh827 <2032951463@qq.com>
2024-11-21 16:31:32 +0800

    commit

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
 //请在下方输入你的代码：
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	for(i=1;i<=9;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			k=i*j;
+			printf("%d*%d=%d",i,j,k);
+			printf("\t");
+		}
+		printf("\n");
+	}
+	return 0;
+}
